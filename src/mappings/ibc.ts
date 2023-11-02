aa39e7d6c591d5bf7b1c316556fb8855e72a67ac
yahyamohmuedpro99 <133171914+yahyamohmuedpro99@users.noreply.github.com>
2023-11-02 19:45:19 +0200

    Create ibc.ts
    add the ibc handler for update client msg

---
+++ src/mappings/ibc.ts
@@ -0,0 +1,11 @@
+import { createTransaction } from './helper'
+import { TOPIC_MESSAGE } from '../common/constants'
+import { MsgUpdateClientMessage } from '../types/CosmosMessageTypes'
+import { sendBatchOfMessagesToKafka } from '../common/kafka-producer'
+
+
+export async function handleMsgUpdateClient(msg: MsgUpdateClientMessage):Promise<void> {
+  const transaction = createTransaction('MsgUpdateClientMessage', msg)
+  logger.info('-------------ibc is work----------------------------')
+  await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+}
