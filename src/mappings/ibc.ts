c2d786d800cbcbe2a35e07a1af2d791b8074b07b
yahyamohmuedpro99 <133171914+yahyamohmuedpro99@users.noreply.github.com>
2023-11-04 20:06:23 +0200

    Update ibc.ts
    remove loggers

--- src/mappings/ibc.ts
+++ src/mappings/ibc.ts
@@ -6,24 +6,19 @@ import { createTransaction } from './helper'
 ​
  export async function handleMsgUpdateClient(msg: MsgUpdateClientMessage):Promise<void> {
    const transaction = createTransaction('MsgUpdateClientMessage', msg)
-   logger.info('-------------ibc is update client----------------------------')
-
    await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
  }
  export async function handleMsgCreateClient(msg: MsgCreateClientMessage):Promise<void> {
   const transaction = createTransaction('MsgUpdateClientMessage', msg)
-  logger.info('-------------ibc is create clint----------------------------')
 
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
 }
 export async function handleMsgTransfer(msg: MsgTransferMessage):Promise<void> {
   const transaction = createTransaction('MsgTransferMessage', msg)
-  logger.info('----// oo \\--**********-------ibc is Transfer init----------------------------')
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
 }
 export async function handleMsgRecvPacket(msg:MsgRecvPacketMessage) {
   const transaction=createTransaction('MsgRecvPacketMessage',msg)
-  logger.info('----// oo \\--**********-------ibc is recived init----------------------------')
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
 
 }
