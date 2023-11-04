09fe5e3d0f2aaab093c2c335b12b4c4880495207
yahyamohmuedpro99 <yahyamohmuedpro99@gmail.com>
2023-11-04 19:30:08 +0200

    this is update with 3 messages of IBC

--- src/mappings/ibc.ts
+++ src/mappings/ibc.ts
@@ -1,11 +1,30 @@
 import { createTransaction } from './helper'
  import { TOPIC_MESSAGE } from '../common/constants'
- import { MsgUpdateClientMessage } from '../types/CosmosMessageTypes'
+ import { MsgUpdateClientMessage ,MsgCreateClientMessage,MsgTransferMessage,MsgRecvPacketMessage} from '../types/CosmosMessageTypes'
  import { sendBatchOfMessagesToKafka } from '../common/kafka-producer'
 ​
 ​
  export async function handleMsgUpdateClient(msg: MsgUpdateClientMessage):Promise<void> {
    const transaction = createTransaction('MsgUpdateClientMessage', msg)
-   logger.info('-------------ibc is work----------------------------')
+   logger.info('-------------ibc is update client----------------------------')
+
    await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
  }
+ export async function handleMsgCreateClient(msg: MsgCreateClientMessage):Promise<void> {
+  const transaction = createTransaction('MsgUpdateClientMessage', msg)
+  logger.info('-------------ibc is create clint----------------------------')
+
+  await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+}
+export async function handleMsgTransfer(msg: MsgTransferMessage):Promise<void> {
+  const transaction = createTransaction('MsgTransferMessage', msg)
+  logger.info('----// oo \\--**********-------ibc is Transfer init----------------------------')
+  await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+}
+export async function handleMsgRecvPacket(msg:MsgRecvPacketMessage) {
+  const transaction=createTransaction('MsgRecvPacketMessage',msg)
+  logger.info('----// oo \\--**********-------ibc is recived init----------------------------')
+}
+
+
+ 
