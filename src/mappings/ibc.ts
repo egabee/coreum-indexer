6001215a2117135e4f9e4a109ccde47268232cef
yahyamohmuedpro99 <133171914+yahyamohmuedpro99@users.noreply.github.com>
2023-11-14 21:44:18 +0200

    Update Coreum indexer with the templete and add the new ibc msgs  (#11)
    * Update project.ts
    
    update new msgs of ibc
    
    * Update ibc.ts
    
    update ibc msgs
    
    * Delete proto/injective directory
    
    not need this proto dir here

--- src/mappings/ibc.ts
+++ src/mappings/ibc.ts
@@ -1,12 +1,13 @@
 import { createTransaction } from './helper'
  import { TOPIC_MESSAGE } from '../common/constants'
- import { MsgUpdateClientMessage ,MsgCreateClientMessage,MsgTransferMessage,MsgRecvPacketMessage,MsgAcknowledgementMessage,MsgVoteMessage} from '../types/CosmosMessageTypes'
+ import { MsgUpdateClientMessage ,MsgCreateClientMessage,MsgTransferMessage,MsgRecvPacketMessage,MsgAcknowledgementMessage,MsgChannelOpenInitMessage,MsgChannelOpenAckMessage,MsgChannelOpenConfirmMessage} from '../types/CosmosMessageTypes'
  import { sendBatchOfMessagesToKafka } from '../common/kafka-producer'
-​
+​import { toJson } from '../common/utils'
 ​
  export async function handleMsgUpdateClient(msg: MsgUpdateClientMessage):Promise<void> {
    const transaction = createTransaction('MsgUpdateClient', msg)
    await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+ 
  }
  export async function handleMsgCreateClient(msg: MsgCreateClientMessage):Promise<void> {
   const transaction = createTransaction('MsgCreateClient', msg)
@@ -17,13 +18,36 @@ export async function handleMsgTransfer(msg: MsgTransferMessage):Promise<void> {
   const transaction = createTransaction('MsgTransfer', msg)
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
 }
-export async function handleMsgRecvPacket(msg:MsgRecvPacketMessage) {
+export async function handleMsgReceivePacket(msg:MsgRecvPacketMessage) {
   const transaction=createTransaction('MsgRecvPacket',msg)
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+
 }
 
 export async function handleMsgAcknowledgement(msg:MsgAcknowledgementMessage):Promise<void> {
   const transaction = createTransaction('MsgAcknowledgement',msg)
   await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+
 }
+// MsgChannelOpenInit ==>/proto/ibc/core/channel/v1/tx.proto
+// MsgChannelOpenAck
+// MsgChannelOpenConfirm
  
+
+export async function handleMsgChannelOpenInit(msg:MsgChannelOpenInitMessage):Promise<void> {
+  const transaction = createTransaction('MsgChannelOpenInit',msg)
+  await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+
+}
+
+export async function handleMsgChannelOpenAck(msg:MsgChannelOpenAckMessage):Promise<void> {
+  const transaction = createTransaction('MsgChannelOpenAck',msg)
+  await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+
+}
+
+export async function handleMsgChannelOpenConfirm(msg:MsgChannelOpenConfirmMessage):Promise<void> {
+  const transaction = createTransaction('MsgChannelOpenConfirm',msg)
+  await sendBatchOfMessagesToKafka([{ messages: [transaction], topic: TOPIC_MESSAGE }])
+
+}
