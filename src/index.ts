69ce830873657e4db201382d3521a8261d0a4cc8
yahyamohmuedpro99 <133171914+yahyamohmuedpro99@users.noreply.github.com>
2023-11-23 22:27:30 +0200

    Update index.ts with the handler
    you don't need all this handler anymore

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import dotenv from 'dotenv'
 dotenv.config({ path: '/app/.env' })
 
 //Exports all handler functions
-export * from './mappings/bank'
-export * from './mappings/contract'
-export * from './mappings/nft'
-export * from './mappings/token'
-export * from './mappings/distribution'
-export * from './mappings/staking'
-export * from './mappings/authz'
-export * from './mappings/ibc'
-export * from './mappings/gov'
+// export * from './mappings/bank'
+// export * from './mappings/contract'
+// export * from './mappings/nft'
+// export * from './mappings/token'
+// export * from './mappings/distribution'
+// export * from './mappings/staking'
+// export * from './mappings/authz'
+// export * from './mappings/ibc'
+// export * from './mappings/gov'
+export * from './mappings/tx'
+
